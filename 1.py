2285f26bd9bf0b6cf4c7b4e117c4edaf4998130f
Nurzhan Nurdaulet <nurzhannurdaulet73@gmail.com>
2025-02-27 18:45:19 +0500

    LAB5

---
+++ 1.py
@@ -0,0 +1,10 @@
+sum = 0
+num = int(input("Enter the number: "))
+for n in range(num):
+    if n % 2 == 1:
+        print(n)
+  
+
+
+
+        
--- my.py
+++ my.py
@@ -1,6 +1,22 @@
-name = str(input())
-new_name = 'k' + name[1:]
-print(new_name)
+num1 = int(input())
+num2 = int(input())
+char = str(input())
+if char == "+":
+    sum = num1 + num2
+    print(num1, num2, sep = "+", end = f"={sum}\n")
+    
 
+elif char == "-":
+    sum = num1 - num2
+    print(num1, num2, sep = "-", end = f"={sum}\n")
+    
 
+elif char == "*":
+    sum = num1 * num2
+    print(num1, num2, sep = "*", end = f"={sum}\n")
+    
 
+elif char == "/":
+    sum = num1 / num2
+    print(num1, num2, sep = "/", end = f"={sum}\n")
+    
